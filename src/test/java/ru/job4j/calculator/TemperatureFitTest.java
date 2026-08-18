80d678fe3b8700e9963dd6666046f5f69f4db04e
ShushakovaA <ya1shusha@yandex.ru>
2026-08-18 19:25:46 +0300

    Что такое import [#505278 #564923] Тестирование Класса Калькулятора Температуры Хранения [#505279]

---
+++ src/test/java/ru/job4j/calculator/TemperatureFitTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.calculator;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+class TemperatureFitTest {
+
+    @Test
+    void whenRoomTemperature10ThenFruits8() {
+        int input = 10;
+        double expected = 8.0;
+        double value = 0.0001;
+        double output = TemperatureFit.idealTemperatureForFruits(input);
+        assertThat(output).isEqualTo(expected, withPrecision(value));
+    }
+
+    @Test
+    void whenRoomTemperature10ThenMeat5() {
+        int input = 10;
+        double expected = 5.0;
+        double value = 0.0001;
+        double output = TemperatureFit.idealTemperatureForMeat(input);
+        assertThat(output).isEqualTo(expected, withPrecision(value));
+    }
+}
